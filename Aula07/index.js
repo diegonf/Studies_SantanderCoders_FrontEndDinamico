36be106541605761709921bf50f44a1c20b553fa
Diego Ferreira <diego.nascimento91@gmail.com>
2023-09-29 22:20:13 -0300

    aula 07 atualizações

--- Aula07/index.js
+++ Aula07/index.js
@@ -6,13 +6,13 @@
 
 // *** Prioridade entra promise, setTimeout, e codigo sincrono
 setTimeout(() => {
-  console.log('setTimeout');
+  console.log('setTimeout'); // isso imprime em terceiro
 }, 0);
 
 const minhaPromiseResolvida = Promise.resolve("Promise");
-minhaPromiseResolvida.then((response) => console.log(response));
+minhaPromiseResolvida.then((response) => console.log(response)); // isso imprime em segundo
 
-console.log('Código sincrono!');
+console.log('Código sincrono!'); // isso imprime primeiro
 
 // ***** Existe diferença de prioridade entre setInterval e setTimeout??
 // setInterval(() => {
